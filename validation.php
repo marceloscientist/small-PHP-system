b3c511c15372536525243c25d8d33c063a53469b
Marcelo de Carvalho Santana <marcelo.scientist@hotmail.com>
2021-01-04 16:38:28 -0300

    some validations only to scrap what is comming next

--- validation.php
+++ validation.php
@@ -1,18 +1,24 @@
 <?php
 
 session_start();
+
+$categorias = array(
+    'infantil',
+     'adolescente', 
+    'adulto'
+);
+
+
 $nome = $_POST['nome'];
-if(empty($nome)) {
-    $_SESSION['message-error'] = "Name's field cannot be empty!";
-    header("location:index.php");
-}
-
 $idade = $_POST['idade'];
-if(empty($idade)) {
-    $_SESSION['message-error'] = "What's your age, please sir!";
-    header("location:index.php");
-}
+
+if(empty($nome)) { $_SESSION['message-error'] = 'O nome não pode ser vazio, por favor preencha-o novamente'; }
+else if(strlen($nome) < 3) { $_SESSION['message-error'] = 'O nome não pode conter menos de 3 caracteres'; }
+else if(strlen($nome) > 40) { $_SESSION['message-error'] = 'O nome não pode conter mais de 40 caracteres'; }
+header("location:index.php");
+
+if(!is_numeric($idade)) { $_SESSION['message-error'] = 'Informe um número para idade'; }
+header("location:index.php");
 
 
 
-echo $nome .' '. $idade;
